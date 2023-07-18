f4f63dc56d7560e8cb8d355545c4ec30ec67abd5
rubimeryka <erykarubim13@gmail.com>
2023-07-18 17:25:34 -0300

    Req07: ting_word_searches - exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = []
+
+    for file in instance._data:
+        name = file["nome_do_arquivo"]
+        lines = file["linhas_do_arquivo"]
+        occurrences = [
+            {"linha": line + 1}
+            for line, line_content in enumerate(lines)
+            if word.lower() in line_content.lower()
+        ]
+
+        if occurrences:
+            files.append(
+                {
+                    "palavra": word,
+                    "arquivo": name,
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return files
 
 
 def search_by_word(word, instance):
