1273dc102af66d9601bbd283a88d0b9327d34b9f
rubimeryka <erykarubim13@gmail.com>
2023-07-18 16:59:27 -0300

    fix lint

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,14 +2,13 @@ import sys
 from ting_file_management.file_management import txt_importer
 
 
-
 def process(path_file, instance):
     for index in range(len(instance)):
         if instance.search(index)["nome_do_arquivo"] == path_file:
             return None
 
     file = txt_importer(path_file)
-    
+
     if file is None:
         return None
 
@@ -23,7 +22,9 @@ def process(path_file, instance):
 
     sys.stdout.write(str(file_data))
 
+
 def remove(instance):
+
     if not instance:
         sys.stdout.write("Não há elementos\n")
         return None
